d9bc516f04767f53a862b0f9c7ed4f27d5504195
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-12-03 16:45:19 -0800

    added privacy policy

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,10 +1,11 @@
 import styles from './Nav.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Nav(){
     return(
         <div className={styles.main}>
-            <Image src='/nav_logo.png' width={170} height={38} />
+            <Link href='/'><Image src='/nav_logo.png' width={170} height={38} /></Link>
         </div>
     )
 }
